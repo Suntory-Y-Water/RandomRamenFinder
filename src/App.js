76d8f9e6b836808bb0a9628b1fcde3a2e1737f2b
sui <1202ayasnppk@gmail.com>
2023-06-10 07:38:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import db from './firebase';
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 function App() {
+  
+  const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // データベースから情報を取得する
+  useEffect( () => {
+    const postData = collection(db, "jirouFromTokyoAndKanagawa");
+    getDocs(postData).then((snapShot) => {
+      const postsData = snapShot.docs.map((doc) => ({ ...doc.data()}));
+      setPosts(postsData);
+    });
+
+    onSnapshot(postData, (post) => {
+      const postsData = post.docs.map((doc) => ({...doc.data()}));
+      setPosts(postsData);
+    });
+  }, [])
+
+  const handleButtonClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      if (posts.length > 0) {
+        const randomIndex = Math.floor(Math.random() * posts.length);
+        setSelectedPost(posts[randomIndex]);
+      }
+      setIsLoading(false);
+    }, 2000);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>じろるーれっと</h1>
+        <div>
+          {isLoading ? <p>読み込み中...</p> :
+            selectedPost && (
+              <div key={selectedPost.store_name}>
+                <h1>{selectedPost.store_name}</h1>
+                <p>{selectedPost.store_address}</p>
+                <p>{selectedPost.open_time}</p>
+                <p>{selectedPost.close_day}</p>
+              </div>
+            )
+          }
+        </div>
+        <div>
+          <button onClick={handleButtonClick} className='randomButton'>ランダムに投稿を表示</button>
+        </div>
+      </div>
+
     </div>
   );
 }
